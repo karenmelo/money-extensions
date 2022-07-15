e6d9b6f63ed38b4b92d6b53d40b215cb944d5e0d
Karen Melo <karen_melo@live.com>
2022-07-15 19:00:13 -0300

    Finalizado a primeira versao

---
+++ MoneyExtensions.Tests/MoneyExtensionsTests.cs
@@ -0,0 +1,14 @@
+namespace MoneyExtensions.Tests;
+
+[TestClass]
+public class MoneyExtensionsTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
---
+++ MoneyExtensions/MoneyExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyExtensions;
+
+public static class Money
+{
+    public static int ToCents(this decimal amount)
+    {
+        if (amount <= 0) return 0;
+
+        var text = amount.ToString("N2").Replace(",", "").Replace(".", ""); //TODO: trocar por um regex
+
+        if (string.IsNullOrEmpty(text)) return 0;
+
+        int.TryParse(text, out var result);
+        return result;
+    }
+}
+
